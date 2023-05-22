1dd497c0a1ae7d71e7d0b105ba68c85a02a25bfb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-22 15:21:54 +0300

    Created the PostsList component

--- src/features/posts/PostsList.jsx
+++ src/features/posts/PostsList.jsx
@@ -1,9 +1,21 @@
 import { useSelector } from 'react-redux'
 
 const PostsList = () => {
-	const posts = useSelector((state) => state.posts)
+    const posts = useSelector((state) => state.posts)
 
-	return <div>PostsList</div>
+	const renderedPosts = posts.map((post) => (
+		<article key={post.id}>
+			<h3>{post.title}</h3>
+			<p>{post.content.substring(0, 100)}</p>
+		</article>
+	))
+
+	return (
+		<section>
+			<h2>Posts</h2>
+			{renderedPosts}
+		</section>
+	)
 }
 
 export default PostsList
