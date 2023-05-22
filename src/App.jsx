3568a754903cd9b73e42818b36af9b15aa46258d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-22 15:04:35 +0300

    Deleted the App css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { useDispatch, useSelector } from 'react-redux'
+import {
+	decrement,
+	increment,
+	incrementByAmount
+} from './features/counter/counterSlice'
+
 function App() {
-	return <main></main>
+	const dispatch = useDispatch()
+	const { count } = useSelector((state) => state.counter)
+
+	return (
+		<main>
+			<p>{count}</p>
+		</main>
+	)
 }
 
 export default App
