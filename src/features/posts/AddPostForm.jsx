f4b7cde8684cfbac19542317a1e59e8f8eccf1a4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-22 16:10:33 +0300

    Reorganized the imports

--- src/features/posts/AddPostForm.jsx
+++ src/features/posts/AddPostForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { postAdded } from './postsSlice'
 import { selectAllUsers } from '../users/usersSlice'
+import { postAdded } from './postsSlice'
 
 const AddPostForm = () => {
 	const dispatch = useDispatch()
