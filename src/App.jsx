7cefe14fa0c9282de9144de0c8eaeb5633fac37c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-22 15:23:49 +0300

    Rendered out the posts list

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 // import { useDispatch, useSelector } from 'react-redux'
 
+import PostsList from './features/posts/PostsList'
+
 function App() {
 	// const dispatch = useDispatch()
 	// const { count } = useSelector((state) => state.counter)
 
-	return <main className='main'></main>
+  return (
+		<main className='main'>
+			<PostsList />
+		</main>
+  )
 }
 
 export default App
