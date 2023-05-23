0465e08637c45daf0185a18ad5f15785a200e1b0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-23 11:49:05 +0300

    Deleted the commented out code

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-// import { useDispatch, useSelector } from 'react-redux'
-
 import PostsList from './features/posts/PostsList'
 
 function App() {
-	// const dispatch = useDispatch()
-	// const { count } = useSelector((state) => state.counter)
-
-  return (
+	return (
 		<main className='main'>
 			<PostsList />
 		</main>
-  )
+	)
 }
 
 export default App
