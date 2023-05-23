a695489d017f0c5ae4834992252291cf7b19f749
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-23 10:56:22 +0300

    Properly dispatched thee fetchUsers async thunk

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { store } from './app/store.js'
 import { fetchUsers } from './features/users/usersSlice.js'
 import './index.css'
 
-store.dispatch(fetchUsers)
+store.dispatch(fetchUsers())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
