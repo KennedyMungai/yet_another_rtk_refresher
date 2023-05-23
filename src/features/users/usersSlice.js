bf2fa5979fccf9821435d8dfb54ffef5fd0672db
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-23 10:51:27 +0300

    Created the fetchUsers async thunk

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -5,6 +5,15 @@ const USERS_URL = 'https://jsonplaceholder.typicode.com/users'
 
 const initialState = []
 
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+	try {
+		const response = await axios.get(USERS_URL)
+		return [...response.data]
+	} catch (error) {
+		return error.message
+	}
+})
+
 const usersSlice = createSlice({
 	name: 'Users',
 	initialState,
