ccee7d2efa9cfc34a06d0933af0617b0e5fbc0b9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-23 09:34:38 +0300

    Created the react emoji object

---
+++ src/features/posts/ReactionButtons.jsx
@@ -0,0 +1,16 @@
+import { useDispatch } from 'react-redux'
+import { reactionAdded } from './postsSlice'
+
+const reactionEmoji = {
+	thumbsUp: '👍',
+	wow: '😲',
+	heart: '❤️',
+	rocket: '🚀',
+	coffee: '☕'
+}
+
+const ReactionButtons = () => {
+	return <div>ReactionButtons</div>
+}
+
+export default ReactionButtons
