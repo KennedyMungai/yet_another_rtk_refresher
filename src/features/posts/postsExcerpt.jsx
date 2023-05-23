9e8258a52b4334c01e8776abda43635b3f3213cd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-23 11:59:57 +0300

    Reduced the number of letters for the excerpt while adding a css class to the paragraph tag

--- src/features/posts/postsExcerpt.jsx
+++ src/features/posts/postsExcerpt.jsx
@@ -7,7 +7,7 @@ const PostsExcerpt = ({ post }) => {
 	return (
 		<article>
 			<h3>{post.title}</h3>
-			<p>{post.body.substring(0, 100)}</p>
+			<p className='excerpt'>{post.body.substring(0, 75)}...</p>
 			<p className='postCredit'>
 				<PostAuthor userId={post.user} />
 				<TimeAgo timestamp={post.date} />
