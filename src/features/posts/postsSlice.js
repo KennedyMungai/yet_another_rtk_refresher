91dca132c3a8091b68639e9f8fbbf865a882731d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-23 10:08:47 +0300

    Imported axios and created a const for the POSTS_URL

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,5 +1,8 @@
 import { createAsyncThunk, createSlice, nanoid } from '@reduxjs/toolkit'
+import axios from 'axios'
 import { sub } from 'date-fns'
+
+const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts'
 
 const initialState = {
 	posts: [],
