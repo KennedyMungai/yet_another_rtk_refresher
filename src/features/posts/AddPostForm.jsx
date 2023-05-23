1f0837c33a5d7d9343e5d3b2de8fae94a66c49d8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-23 11:05:27 +0300

    Created the addRequestStatus state

--- src/features/posts/AddPostForm.jsx
+++ src/features/posts/AddPostForm.jsx
@@ -1,32 +1,33 @@
 import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { selectAllUsers } from '../users/usersSlice'
-import { postAdded } from './postsSlice'
+import { addNewPost } from './postsSlice'
 
 const AddPostForm = () => {
 	const dispatch = useDispatch()
 	const users = useSelector(selectAllUsers)
 
 	const [title, setTitle] = useState('')
-    const [content, setContent] = useState('')
+	const [content, setContent] = useState('')
 	const [userId, setUserId] = useState('')
+	const [addRequestStatus, setAddRequestStatus] = useState('idle')
 
 	const onTitleChanged = (e) => setTitle(e.target.value)
-    const onContentChanged = (e) => setContent(e.target.value)
+	const onContentChanged = (e) => setContent(e.target.value)
 	const onAuthorChanged = (e) => setUserId(e.target.value)
 
 	const onSavePostClicked = (e) => {
 		e.preventDefault()
 
 		if (title && content) {
-			dispatch(postAdded(title, content, userId))
+			dispatch(addNewPost({ title, content, userId }))
 		}
 
 		setTitle('')
 		setContent('')
 	}
-    
-    const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
+
+	const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
 
 	const userOptions = users.map((user) => (
 		<option key={user.id} value={user.id}>
