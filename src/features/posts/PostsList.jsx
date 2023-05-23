517e8223bfa0df16dd2b8e026f5ce45009413ef9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-23 10:29:19 +0300

    Used useSeelector ro access the errors and status states

--- src/features/posts/PostsList.jsx
+++ src/features/posts/PostsList.jsx
@@ -15,6 +15,8 @@ const PostsList = () => {
 	const dispatch = useDispatch()
 	
 	const posts = useSelector(selectAllPosts)
+	const errors = useSelector(getPostsError)
+	const status = useSelector(getPostsStatus)
 
 	const orderedPosts = posts
 		.slice()
