186cd6a67a996896105611737353b97d339a4ba6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-23 11:33:01 +0300

    Added some basic logic to the component

--- src/features/posts/SinglePostPage.jsx
+++ src/features/posts/SinglePostPage.jsx
@@ -1,4 +1,22 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { selectPostById } from './postsSlice'
+import PostAuthor from './PostAuthor'
+import TimeAgo from './TimeAgo'
+import ReactionButtons from './ReactionButtons'
+
 const SinglePostPage = () => {
+	// Retrieve the post id
+
+	const post = useSelector((state) => selectPostById(state, postId))
+
+	if (!post) {
+		return (
+			<section>
+				<h2>Post Not Found</h2>
+			</section>
+		)
+	}
+
 	return <div>SinglePostPage</div>
 }
 
