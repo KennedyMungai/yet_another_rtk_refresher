3b781aa4ddb56a3831f9068fe4f9d80f58b004ec
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-23 11:46:03 +0300

    Reorganized the imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import App from './App.jsx'
 import { store } from './app/store.js'
 import { fetchUsers } from './features/users/usersSlice.js'
 import './index.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 store.dispatch(fetchUsers())
 
